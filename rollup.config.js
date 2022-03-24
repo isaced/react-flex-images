7560c87fd698a1ee00352ce91b48ec3187f3346e
isaced <isaced@163.com>
2022-03-24 23:28:15 +0800

    chore: use terser to minify

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,37 @@ import typescript from "@rollup/plugin-typescript";
 import serve from "rollup-plugin-serve";
 import replace from "@rollup/plugin-replace";
 import styles from "rollup-plugin-styles";
+import { terser } from "rollup-plugin-terser";
 
 const dev = process.env.NODE_ENV === "development";
 
-export default [
-  {
+const config = [];
+
+// production build
+
+!dev &&
+  config.push({
     input: "src/index.ts",
     output: {
       name: "react-flex-images",
       file: "dist/index.js",
       format: "umd",
     },
-    plugins: [commonjs(), typescript(), styles()],
-  },
-  {
+    plugins: [
+      commonjs(),
+      typescript(),
+      styles(),
+      terser({
+        output: {
+          comments: false,
+        },
+      }),
+    ],
+  });
+
+// demo
+dev &&
+  config.push({
     input: "demo/index.tsx",
     output: {
       file: "build/demo.js",
@@ -31,7 +48,9 @@ export default [
       commonjs(),
       typescript(),
       styles(),
-      dev ? serve(["build", "demo"]) : null,
+      terser(),
+      serve(["build", "demo"]),
     ],
-  },
-];
+  });
+
+export default config;
